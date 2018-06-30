f8bbc860f4880be2b8ddc6fda41ae173d68d0e37
Gabriel Valle <gvalle91@gmail.com>
2018-06-30 17:18:23 +0000

    automatically backed up by learn

--- db/migrate/20180630171553_create_movies.rb
+++ db/migrate/20180630171553_create_movies.rb
@@ -1,4 +1,16 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
   end
 end
+itle: "The Sting",
+release_date: 1973,
+director: "George Roy Hill",
+lead: "Paul Newman",
+in_theaters: false
